cac75b135953dca12dc969895f743b1bc3958357
Muhammad Ahmed <ahmedsilat95@gmail.com>
2023-11-23 23:42:51 +0500

    camera-and-local-storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAONvFskZKoFQtSfv0ETQsywidt8AfXO08",
-    authDomain: "dsl-bidding-system.firebaseapp.com",
-    projectId: "dsl-bidding-system",
-    storageBucket: "dsl-bidding-system.appspot.com",
-    messagingSenderId: "783731630179",
-    appId: "1:783731630179:web:0c6f627fc461884611d856",
-    measurementId: "G-7FS432D6YL"
+  apiKey: "AIzaSyAONvFskZKoFQtSfv0ETQsywidt8AfXO08",
+  authDomain: "dsl-bidding-system.firebaseapp.com",
+  projectId: "dsl-bidding-system",
+  storageBucket: "dsl-bidding-system.appspot.com",
+  messagingSenderId: "783731630179",
+  appId: "1:783731630179:web:0c6f627fc461884611d856",
+  measurementId: "G-7FS432D6YL",
 };
 
 const app = initializeApp(firebaseConfig);
 var db = getFirestore(app);
+const storage = getStorage(app);
 
-export {db}
+export { db, storage };
